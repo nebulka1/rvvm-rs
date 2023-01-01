1f3976093c00360a3f047d48da97d3fb791534d2
Nero <resqnetm@gmail.com>
2023-01-01 20:13:20 +0300

    Fix LICENSE & update manifests

--- bin/main.rs
+++ bin/main.rs
@@ -1,33 +1,39 @@
 use rvvm::prelude::*;
 
-// #[on_remove(ty = "()")]
-// fn test(dev: &mut Device<()>) {
-//     println!("I AM REMOVED!!!");
-// }
+#[type_handler(ty = "()")]
+fn on_reset(dev: &Device<()>) {
+    println!("Reset");
+}
+
+#[type_handler(ty = "()")]
+fn on_update(dev: &Device<()>) {
+    println!("Update");
+}
+
+#[on_remove(ty = "()")]
+fn on_remove(dev: &mut Device<()>) {
+    println!("I'm being removed");
+}
 
 fn main() {
+    // Creates the RVVM instance with the 4KB of memory and 1
+    // hart, see `rvvm::builders::instance::InstanceBuilder`
+    // documentation for more detailed information or use the
+    // `rvvm::instance::Instance::new` for direct creation
     let mut instance = Instance::builder().build();
-    let uart_ty = DeviceType::custom(
-        "UART",
-        RemoveHandler::none(),
-        TypeHandler::none(),
-        TypeHandler::none(),
-    );
-    let uart = Device::builder()
+
+    let test_type =
+        DeviceType::custom("Test", on_remove, on_update, on_reset);
+    let test_dev = Device::builder()
         .address(0x1024)
         .size(1024)
-        .data(())
-        .device_type(uart_ty)
-        .op_size(1..=1)
+        .device_type(test_type)
+        .op_size(1..=1) // inclusive range [from; to]
+        .data(()) // Data for ZST types will not be allocated, but dropped
         .build();
+    let handle: DeviceHandle<()> = instance
+        .try_attach_device(test_dev)
+        .expect("Failed to attach MMIO device");
 
-    match instance.try_attach_device(uart) {
-        Ok(handle) => {
-            println!("Attached UART: {handle:?}");
-        }
-
-        Err(e) => {
-            eprintln!("Failed to attach UART: {e}");
-        }
-    }
+    dbg!(handle);
 }
