f0a33d72865e5730bc5e28c4916978921974f1f3
Nero <resqnetm@gmail.com>
2023-01-07 00:28:49 +0300

    Bump integral-enum version

--- src/error.rs
+++ src/error.rs
@@ -2,42 +2,36 @@ use integral_enum::IntegralEnum;
 use thiserror::Error;
 
 #[derive(IntegralEnum, Error)]
-#[enum_disable(display)]
 pub enum InstanceCreateError {
     #[error("Failed to allocate memory for the instance")]
     FailedToAllocate,
 }
 
 #[derive(IntegralEnum, Error)]
-#[enum_disable(display)]
 pub enum InstancePauseError {
     #[error("Instance is not running")]
     NotRunning,
 }
 
 #[derive(IntegralEnum, Error)]
-#[enum_disable(display)]
 pub enum InstanceStartError {
     #[error("Instance is already running")]
     AlreadyRunning,
 }
 
 #[derive(IntegralEnum, Error)]
-#[enum_disable(display)]
 pub enum DtbDumpError {
     #[error("Failed to open destination file for write")]
     FailedToOpenFile,
 }
 
 #[derive(IntegralEnum, Error)]
-#[enum_disable(display)]
 pub enum MemoryAccessError {
     #[error("Requested range is out-of-bounds")]
     OutOfBounds,
 }
 
 #[derive(IntegralEnum, Error)]
-#[enum_disable(display)]
 pub enum DeviceAttachError {
     #[error("Tried to attach device to already occupied region")]
     RegionIsOccupied,
--- src/fdt/error.rs
+++ src/fdt/error.rs
@@ -3,5 +3,6 @@ use thiserror::Error;
 
 #[derive(Error, IntegralEnum)]
 pub enum SerializeError {
+    #[error("Insufficient space for serialization")]
     InsufficientSpace,
 }
